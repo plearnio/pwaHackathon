2c48b407b1d954450f672a01cfa0db89c8542599
plearnio <p.plearn.io@gmail.com>
2017-06-25 00:06:50 +0700

    Inrest react app and test deploy with firebase

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Push si !</h2>
+    </div>
+    <p className="App-intro">
+      Test react deploy
+    </p>
+  </div>
+)
+
+export default App;
